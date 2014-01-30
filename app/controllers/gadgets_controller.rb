05be2c75808528d25465fae519b55e63b0c151ea
Christian Rijke <crijke@gmail.com>
2014-01-30 17:42:36 +0100

    Implement gadget resource and controller.

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,2 +1,28 @@
-class GadgetsController < InheritedResources::Base
+class GadgetsController < ApplicationController
+
+  def index
+    @gadgets = Gadget.all
+  end
+
+  def new
+    @gadget = Gadget.new
+  end
+
+  def create
+    @gadget = Gadget.new(gadget_params)
+
+    respond_to do |format|
+      if @gadget.save
+        format.html { redirect_to @gadget, notice: 'Gadget was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+
+  def gadget_params
+    params.require(:gadget).permit(:name, :text)
+  end
 end
