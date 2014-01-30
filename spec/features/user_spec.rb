2658c822074d6a05db2eb6590a722625eeff4ff6
Christian Rijke <crijke@gmail.com>
2014-01-30 16:44:03 +0100

    Add user account spec.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,27 +4,26 @@ feature 'User Account' do
   scenario "user logs in" do
     visit('/')
     click_link('login')
-    fill_in('email', with: 'admin@example.com')
-    fill_in('password', with: 'password')
-    expect(page).to have_conent('You are logged in: admin@example.com')
+    fill_in('user_email', with: 'admin@example.com')
+    fill_in('user_password', with: 'password')
+    expect(page).to have_content('You are logged in as admin@example.com')
   end
 
   scenario "user signs up" do
     visit('/')
-    click_link('login')
-    fill_in('email', with: 'admin@example.com')
-    fill_in('password', with: 'password')
-    fill_in('password_confirmation', with: 'password')
-    expect(page).to have_conent('You are logged in: admin@example.com')
+    click_link('Sign up')
+    fill_in('user_email', with: 'admin@example.com')
+    fill_in('user_password', with: 'password')
+    fill_in('user_password_confirmation', with: 'password')
+    expect(page).to have_content('You are logged in as admin@example.com')
   end
 
   scenario "user logs out" do
     visit('/')
     click_link('login')
-    fill_in('email', with: 'admin@example.com')
-    fill_in('password', with: 'password')
-    click_link('logout')
-    expect(page).to have_conent('Please log in.')
-
+    fill_in('user_email', with: 'admin@example.com')
+    fill_in('user_password', with: 'password')
+    click_link('Sign in')
+    expect(page).to have_content('Please log in.')
   end
 end
