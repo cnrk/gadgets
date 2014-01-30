863a0898966b766fc75bcbe9004c181e11287f1d
Christian Rijke <crijke@gmail.com>
2014-01-30 21:01:02 +0100

    Implement proper user account spec.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,33 +1,61 @@
 require 'spec_helper'
 
 feature 'User Account' do
-  scenario "user logs in" do
-    pending
-    visit('/')
-    click_link('login')
-    fill_in('user_email', with: 'admin@example.com')
-    fill_in('user_password', with: 'password')
-    click_button('Sign in')
-    expect(page).to have_content('logout')
-  end
+  let(:user){ FactoryGirl.create(:user) }
 
-  scenario "user signs up" do
-    pending
+  scenario "user signs up with correct credentials" do
     visit('/')
     click_link('sign up')
-    fill_in('user_email', with: 'admin@example.com')
-    fill_in('user_password', with: 'password')
-    fill_in('user_password_confirmation', with: 'password')
+    fill_in('user_email', with: 'admin2@example.com')
+    fill_in('user_password', with: 'admin2admin2')
+    fill_in('user_password_confirmation', with: 'admin2admin2')
+    click_button('Sign up')
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  scenario "user signs up with incorrect credentials" do
+    visit('/')
+    click_link('sign up')
+    fill_in('user_email', with: 'admin2@example.com')
+    fill_in('user_password', with: 'admin2admin2')
+    fill_in('user_password_confirmation', with: 'wrongpassword')
+    click_button('Sign up')
+    expect(page).to have_content("Password confirmation doesn't match Password")
+    expect(page).to have_content('login')
+  end
+
+  scenario "user logs in with correct credentials" do
+    visit('/')
+    click_link('login')
+    fill_in('user_email', with: user.email)
+    fill_in('user_password', with: user.password)
+    click_button('Sign in')
+    expect(page).to have_content('Signed in successfully.')
     expect(page).to have_content('logout')
   end
 
-  scenario "user logs out" do
-    pending
+  scenario "user logs in with incorrect credentials" do
     visit('/')
     click_link('login')
-    fill_in('user_email', with: 'admin@example.com')
-    fill_in('user_password', with: 'password')
+    fill_in('user_email', with: user.email)
+    fill_in('user_password', with: 'wrongpassword')
     click_button('Sign in')
+    expect(page).to have_content('Invalid email or password.')
     expect(page).to have_content('login')
+  end
+
+  context "user is logged in" do
+    before do
+      visit('/')
+      click_link('login')
+      fill_in('user_email', with: user.email)
+      fill_in('user_password', with: user.password)
+      click_button('Sign in')
+    end
+
+    scenario "user logs out" do
+      click_link('logout')
+      expect(page).to have_content('login')
+    end
   end
 end
