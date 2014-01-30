fad62ffb9d5c0a426d0101e2002f7afacff3ae6c
Christian Rijke <crijke@gmail.com>
2014-01-30 22:06:20 +0100

    Add welcome text.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Home Page' do
   scenario "user visits the home page" do
     visit('/')
-    expect(page).to have_content('Gadgets')
+    expect(page).to have_content('Welcome to Gadgets')
   end
 end
 
