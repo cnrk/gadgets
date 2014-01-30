f8fef35c319be32f7a252a445312c9293fbe8aa6
Christian Rijke <crijke@gmail.com>
2014-01-30 21:28:52 +0100

    Fix gadgets model spec.

--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
 describe Gadget do
-  let(:gadget){ FactoryGirl.build(:gadget) }
+  let(:gadget){ build(:gadget) }
 
-  it "should be a valid" do
-    pending 'factory girl not properly initialized'
+  it 'should be a valid' do
     expect(gadget).to be_valid
   end
 
-  it "requires a name" do
-    pending 'factory girl not properly initialized'
+  it 'requires a name' do
     gadget.name = nil
     expect(gadget).not_to be_valid
   end
